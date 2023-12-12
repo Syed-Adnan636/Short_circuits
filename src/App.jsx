65692f04aa9301a6145503b09902cc6ddd450769
Syed Adnan <syedadnansafdar6369@gmail.com>
2023-12-12 22:34:54 +0500

    Short Circuits

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShortCircuits from './ShortCircuits/ShortCircuits'
+
+const App = () => {
+  return (
+    <>
+      <ShortCircuits/>
+    </>
+  )
+}
+
+export default App
